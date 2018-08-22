32e92369e7c2b308faf0970ad1ea78e9be4cc586
Albert <31998800+seuraltimez@users.noreply.github.com>
2018-08-22 09:37:35 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,70 +8,67 @@ $(".mat-input").focusout(function(){
   $(this).parent().removeClass("is-active");
 })
 
-	$(document).ready(function(){
-		    $("#menu").on("click","a", function (event) {
-		        event.preventDefault();
-		        var id  = $(this).attr('href'),
-		            top = $(id).offset().top;
-		        $('body,html').animate({scrollTop: top}, 1000);
-		    });
-		});
+$(document).ready(function(){
+  $('.slider').bxSlider();
+});
+
+$(document).ready(function(){
+$("a[href*=#]").on("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+});
+});
 
 
-	var header = new Headhesive('header');	
-
-
-
+var header = new Headhesive('header');	
+$('.navbar-nav a').on('click', function() {
+    if ($('.navbar-toggler').css('display') != 'none') {
+    $(".navbar-toggler").trigger("click");
+}
+});
 
 
 $(function() {
-    var num = $(".number");
-    num.each(function(indx, el) {
-        var max = $(el).data("max");
-        var duration = 5000;
-        var visibility = checkViewport(el);
-        $(el).on("animeNum", function() {
-            $({n: 0}).animate({n: max}, {
-                easing: "linear",
-                duration: duration,
-                step: function(now, fx) {
-                    $(el).html(now | 0)
-                }
-            })
-        }).data("visibility", visibility);
-        visibility && $(el).trigger("animeNum")
-    });
-
-    function checkViewport(el) {
-    var H = document.documentElement.clientHeight,
-        h = el.scrollHeight,
-        pos = el.getBoundingClientRect();
-        return pos.top + h > 0 && pos.bottom - h < H
-    }
-    $(window).scroll(function() {
-        num.each(function(indx, el) {
-            var visibility = checkViewport(el);
-            el = $(el);
-            var old = el.data("visibility");
-            old != visibility && el.data("visibility", visibility) && !old && el.trigger("animeNum")
+var num = $(".number");
+num.each(function(indx, el) {
+    var max = $(el).data("max");
+    var duration = 5000;
+    var visibility = checkViewport(el);
+    $(el).on("animeNum", function() {
+        $({n: 0}).animate({n: max}, {
+            easing: "linear",
+            duration: duration,
+            step: function(now, fx) {
+                $(el).html(now | 0)
+            }
         })
-    })
+    }).data("visibility", visibility);
+    visibility && $(el).trigger("animeNum")
 });
 
-
-
+function checkViewport(el) {
+var H = document.documentElement.clientHeight,
+    h = el.scrollHeight,
+    pos = el.getBoundingClientRect();
+    return pos.top + h > 0 && pos.bottom - h < H
+}
 $(window).scroll(function() {
-  $('.navbar-collapse.in').collapse('hide');
+    num.each(function(indx, el) {
+        var visibility = checkViewport(el);
+        el = $(el);
+        var old = el.data("visibility");
+        old != visibility && el.data("visibility", visibility) && !old && el.trigger("animeNum")
+    })
+})
 });
-$('#navbarNav a').click(function() {
-  $('#navbarNav').collapse('hide');
-});
-
 
 $(function() {  
     jQuery.scrollSpeed(100, 800);
 });
-
 
 ;( function( $ ) {
 
@@ -85,16 +82,3 @@ $(function() {
  } );
 
 } )( jQuery );
-
-$(function() {
-    var selectedClass = "";
-    $(".filter").click(function(){
-        selectedClass = $(this).attr("data-rel");
-        $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-        setTimeout(function() {
-            $("."+selectedClass).fadeIn().addClass('animation');
-            $("#gallery").fadeTo(300, 1);
-        }, 300);
-    });
-});
